651da5efac28dd1dd9ce80d03cb85e5900433028
NexaDev-26 <64757038+NexaDev-26@users.noreply.github.com>
2025-11-20 23:34:08 +0300

    Update ThemeContext.js

--- frontend/src/context/ThemeContext.js
+++ frontend/src/context/ThemeContext.js
@@ -1,15 +1,7 @@
 import React, { createContext, useState, useContext } from 'react';
-
 const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
-
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
-  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
-
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <div className={theme}>{children}</div>
-    </ThemeContext.Provider>
-  );
+  const [theme] = useState('light');
+  return <ThemeContext.Provider value={{ theme }}>{children}</ThemeContext.Provider>;
 };
